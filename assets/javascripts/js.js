fbedfd8e8d9739424b8b52fe5afb91de17a96952
Alexis Duran <alexis@hashlabs.com>
2014-03-12 17:33:02 -0430

    JS for transition while scrolling

--- assets/javascripts/js.js
+++ assets/javascripts/js.js
@@ -1,2 +1,13 @@
-$(function() {
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 150) {
+      $('.logotype').show();
+      $('.side-nav__button').css({'opacity': '1'});
+    } else {
+      $('.logotype').hide();
+      $('.side-nav__button').css({'opacity': '.3'});
+    }
+  });  
+
 });
