73b370ad02b0a5d11f0c37003a3caaab0c5352f2
Alexis Duran <alexis@hashlabs.com>
2014-03-14 12:38:34 -0430

    Add animation for button nav on IPhone

--- assets/javascripts/js.js
+++ assets/javascripts/js.js
@@ -1,4 +1,14 @@
+functions = {
+    setButtonNav : function () {
+        var windowWidth = $(window).width(),
+            buttonNav = $('.side-nav__button');
+
+        buttonNav.addClass('on-mobile');
+    }
+};
+
 $(document).ready(function () {
+    functions.setButtonNav();
 
     $('.side-nav-container').hover(function () {
         $(this).addClass('is-showed');
@@ -11,24 +21,39 @@ $(document).ready(function () {
     $(window).scroll(function () {
         var logotype = $('.logotype'),
             buttonNav = $('.side-nav__button'),
+            buttonNavMobile = buttonNav.filter('.on-mobile'),
             scrollTop = $(window).scrollTop(),
-            windowHeight = $(window).outerHeight(),
-            kudoSide = $('.kudo');
-            kudoBottom = $('.instapaper'); // $('.kudo-bottom')
-            kudoBottomPosition = kudoBottom.offset().top; // instapaper for now while adding kudo-bottom elem
+            windowHeight = $(window).height(),
+            kudoSide = $('.kudo'),
+            kudoBottom = $('.instapaper'), // $('.kudo-bottom')
+            kudoBottomPosition = false;
+
+            if (kudoBottom.length > 0) {
+                kudoBottomPosition = kudoBottom.offset().top; // instapaper for now while adding kudo-bottom elem
+            }
 
         if ( scrollTop > 150) {
             logotype.addClass('is-showed');
             buttonNav.addClass('no-opacity');
+            if ( buttonNavMobile.length > 0) {
+                buttonNavMobile.hide();
+            }
         } else {
             logotype.removeClass('is-showed');
-            buttonNav.removeClass('no-opacity')
+            buttonNav.removeClass('no-opacity');
+            if ( buttonNavMobile.length > 0) {
+                buttonNavMobile.show();
+            }
         }
 
-        if ( scrollTop + windowHeight > kudoBottomPosition) {
+        if (kudoBottomPosition !== false && ( scrollTop + windowHeight > kudoBottomPosition)) {
             kudoSide.addClass('hide');
         } else {
             kudoSide.removeClass('hide');   
         }
-    });  
+    });
+
+    $(window).resize(function () {
+        functions.setButtonNav();
+    });
 });
