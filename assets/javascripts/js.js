350209bd9bebca2d9365e531d87ecdf53758026d
Alexis Duran <alexis@hashlabs.com>
2014-03-14 11:46:28 -0430

    Add animation for kudo-side when kudo-bottom is showed

--- assets/javascripts/js.js
+++ assets/javascripts/js.js
@@ -1,24 +1,34 @@
-$(document).ready(function() {
-  
-  $('.side-nav-container').hover(function() {
-    $(this).addClass('is-showed');
-    $('.kudo').addClass('hide');
-  }, function() {
-    $(this).removeClass('is-showed');
-    $('.kudo').removeClass('hide');
-  });
+$(document).ready(function () {
 
-  $(window).scroll(function () {
-    var logotype = $('.logotype');
-    var buttonNav = $('.side-nav__button');
+    $('.side-nav-container').hover(function () {
+        $(this).addClass('is-showed');
+        $('.kudo').addClass('hide');
+    }, function() {
+        $(this).removeClass('is-showed');
+        $('.kudo').removeClass('hide');
+    });
 
-    if ($(window).scrollTop() > 150) {
-      logotype.addClass('is-showed');
-      buttonNav.addClass('no-opacity');
-    } else {
-      logotype.removeClass('is-showed');
-      buttonNav.removeClass('no-opacity')
-    }
-  });  
+    $(window).scroll(function () {
+        var logotype = $('.logotype'),
+            buttonNav = $('.side-nav__button'),
+            scrollTop = $(window).scrollTop(),
+            windowHeight = $(window).outerHeight(),
+            kudoSide = $('.kudo');
+            kudoBottom = $('.instapaper'); // $('.kudo-bottom')
+            kudoBottomPosition = kudoBottom.offset().top; // instapaper for now while adding kudo-bottom elem
 
+        if ( scrollTop > 150) {
+            logotype.addClass('is-showed');
+            buttonNav.addClass('no-opacity');
+        } else {
+            logotype.removeClass('is-showed');
+            buttonNav.removeClass('no-opacity')
+        }
+
+        if ( scrollTop + windowHeight > kudoBottomPosition) {
+            kudoSide.addClass('hide');
+        } else {
+            kudoSide.removeClass('hide');   
+        }
+    });  
 });
