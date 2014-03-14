29d1c2b687fca464b3982c47a65fe58844d16c5a
Alexis Duran <alexis@hashlabs.com>
2014-03-14 11:01:29 -0430

    Kudo animation for hide when side-nav came out

--- assets/javascripts/js.js
+++ assets/javascripts/js.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   
+  $('.side-nav-container').hover(function() {
+    $(this).addClass('is-showed');
+    $('.kudo').addClass('hide');
+  }, function() {
+    $(this).removeClass('is-showed');
+    $('.kudo').removeClass('hide');
+  });
+
   $(window).scroll(function () {
     var logotype = $('.logotype');
     var buttonNav = $('.side-nav__button');
